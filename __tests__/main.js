8adb53335793797abe676ec50c4e62f8faf04fd2
Benjie Gillam <benjie@jemjie.com>
2020-01-17 10:50:21 +0000

    feat: simple collections support (#10)

--- __tests__/main.js
+++ __tests__/main.js
@@ -33,6 +33,7 @@ let pgPool;
 let schema;
 const options = {
   appendPlugins: [require("..")],
+  simpleCollections: "both",
 };
 beforeAll(() => {
   pgPool = new pg.Pool({
@@ -70,240 +71,440 @@ function check(query, expected) {
     );
 }
 
-describe("parents", () => {
-  test(
-    "Omits archived parents by default",
-    check(
-      `{
-      allParents {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allParents: { nodes: iderize(1) } },
-    ),
-  );
+describe("connections", () => {
+  describe("parents", () => {
+    test(
+      "Omits archived parents by default",
+      check(
+        `{
+          allParents {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allParents: { nodes: iderize(1) } },
+      ),
+    );
 
-  test(
-    "Omits archived parents when NO",
-    check(
-      `{
-      allParents(includeArchived: NO) {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allParents: { nodes: iderize(1) } },
-    ),
-  );
+    test(
+      "Omits archived parents when NO",
+      check(
+        `{
+          allParents(includeArchived: NO) {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allParents: { nodes: iderize(1) } },
+      ),
+    );
 
-  test(
-    "Includes everything when YES",
-    check(
-      `{
-      allParents(includeArchived: YES) {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allParents: { nodes: iderize(1, 2) } },
-    ),
-  );
+    test(
+      "Includes everything when YES",
+      check(
+        `{
+          allParents(includeArchived: YES) {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allParents: { nodes: iderize(1, 2) } },
+      ),
+    );
 
-  test(
-    "Includes only archived when EXCLUSIVELY",
-    check(
-      `{
-      allParents(includeArchived: EXCLUSIVELY) {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allParents: { nodes: iderize(2) } },
-    ),
-  );
-});
+    test(
+      "Includes only archived when EXCLUSIVELY",
+      check(
+        `{
+          allParents(includeArchived: EXCLUSIVELY) {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allParents: { nodes: iderize(2) } },
+      ),
+    );
+  });
 
-describe("children", () => {
-  test(
-    "Omits archived children by default",
-    check(
-      `{
-      allChildren {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allChildren: { nodes: iderize(1001, 2001) } },
-    ),
-  );
+  describe("children", () => {
+    test(
+      "Omits archived children by default",
+      check(
+        `{
+          allChildren {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allChildren: { nodes: iderize(1001, 2001) } },
+      ),
+    );
 
-  test(
-    "Omits archived children when NO",
-    check(
-      `{
-      allChildren(includeArchived: NO) {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allChildren: { nodes: iderize(1001, 2001) } },
-    ),
-  );
+    test(
+      "Omits archived children when NO",
+      check(
+        `{
+          allChildren(includeArchived: NO) {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allChildren: { nodes: iderize(1001, 2001) } },
+      ),
+    );
 
-  test(
-    "Includes everything when YES",
-    check(
-      `{
-      allChildren(includeArchived: YES) {
-        nodes {
-          id
-        }
-      }
-    }`,
-      {
-        allChildren: {
-          nodes: iderize(1001, 1002, 2001, 2002),
+    test(
+      "Includes everything when YES",
+      check(
+        `{
+          allChildren(includeArchived: YES) {
+            nodes {
+              id
+            }
+          }
+        }`,
+        {
+          allChildren: {
+            nodes: iderize(1001, 1002, 2001, 2002),
+          },
         },
-      },
-    ),
-  );
+      ),
+    );
 
-  test(
-    "Includes only archived when EXCLUSIVELY",
-    check(
-      `{
-      allChildren(includeArchived: EXCLUSIVELY) {
-        nodes {
-          id
-        }
-      }
-    }`,
-      { allChildren: { nodes: iderize(1002, 2002) } },
-    ),
-  );
-});
+    test(
+      "Includes only archived when EXCLUSIVELY",
+      check(
+        `{
+          allChildren(includeArchived: EXCLUSIVELY) {
+            nodes {
+              id
+            }
+          }
+        }`,
+        { allChildren: { nodes: iderize(1002, 2002) } },
+      ),
+    );
+  });
 
-describe("children of parents", () => {
-  test(
-    "Omits archived parents and children by default",
-    check(
-      `{
-        allParents {
-          nodes {
-            id
-            childrenByParentId {
-              nodes {
-                id
+  describe("children of parents", () => {
+    test(
+      "Omits archived parents and children by default",
+      check(
+        `{
+          allParents {
+            nodes {
+              id
+              childrenByParentId {
+                nodes {
+                  id
+                }
               }
             }
           }
-        }
-      }`,
-      {
-        allParents: {
-          nodes: [
+        }`,
+        {
+          allParents: {
+            nodes: [
+              {
+                id: 1,
+                childrenByParentId: { nodes: iderize(1001) },
+              },
+            ],
+          },
+        },
+      ),
+    );
+
+    test(
+      "Omits archived parents and children when NO",
+      check(
+        `{
+          allParents(includeArchived: NO) {
+            nodes {
+              id
+              childrenByParentId {
+                nodes {
+                  id
+                }
+              }
+            }
+          }
+        }`,
+        {
+          allParents: {
+            nodes: [
+              {
+                id: 1,
+                childrenByParentId: { nodes: iderize(1001) },
+              },
+            ],
+          },
+        },
+      ),
+    );
+
+    test(
+      "Includes all parents, and treats children as INHERIT (all children of an archived parent, but only the unarchived children of an unarchived parent) when YES",
+      check(
+        `{
+          allParents(includeArchived: YES) {
+            nodes {
+              id
+              childrenByParentId {
+                nodes {
+                  id
+                }
+              }
+            }
+          }
+        }`,
+        {
+          allParents: {
+            nodes: [
+              {
+                id: 1,
+                childrenByParentId: { nodes: iderize(1001) },
+              },
+              {
+                id: 2,
+                childrenByParentId: { nodes: iderize(2001, 2002) },
+              },
+            ],
+          },
+        },
+      ),
+    );
+
+    test(
+      "Includes only archived parents (and all their children) when EXCLUSIVELY",
+      check(
+        `{
+          allParents(includeArchived: EXCLUSIVELY) {
+            nodes {
+              id
+              childrenByParentId {
+                nodes {
+                  id
+                }
+              }
+            }
+          }
+        }`,
+        {
+          allParents: {
+            nodes: [
+              {
+                id: 2,
+                childrenByParentId: { nodes: iderize(2001, 2002) },
+              },
+            ],
+          },
+        },
+      ),
+    );
+  });
+});
+
+describe("simple collections", () => {
+  describe("parents", () => {
+    test(
+      "Omits archived parents by default",
+      check(
+        `{
+          allParentsList {
+            id
+          }
+        }`,
+        { allParentsList: iderize(1) },
+      ),
+    );
+
+    test(
+      "Omits archived parents when NO",
+      check(
+        `{
+          allParentsList(includeArchived: NO) {
+            id
+          }
+        }`,
+        { allParentsList: iderize(1) },
+      ),
+    );
+
+    test(
+      "Includes everything when YES",
+      check(
+        `{
+          allParentsList(includeArchived: YES) {
+            id
+          }
+        }`,
+        { allParentsList: iderize(1, 2) },
+      ),
+    );
+
+    test(
+      "Includes only archived when EXCLUSIVELY",
+      check(
+        `{
+          allParentsList(includeArchived: EXCLUSIVELY) {
+            id
+          }
+        }`,
+        { allParentsList: iderize(2) },
+      ),
+    );
+  });
+
+  describe("children", () => {
+    test(
+      "Omits archived children by default",
+      check(
+        `{
+          allChildrenList {
+            id
+          }
+        }`,
+        { allChildrenList: iderize(1001, 2001) },
+      ),
+    );
+
+    test(
+      "Omits archived children when NO",
+      check(
+        `{
+          allChildrenList(includeArchived: NO) {
+            id
+          }
+        }`,
+        { allChildrenList: iderize(1001, 2001) },
+      ),
+    );
+
+    test(
+      "Includes everything when YES",
+      check(
+        `{
+          allChildrenList(includeArchived: YES) {
+            id
+          }
+        }`,
+        {
+          allChildrenList: iderize(1001, 1002, 2001, 2002),
+        },
+      ),
+    );
+
+    test(
+      "Includes only archived when EXCLUSIVELY",
+      check(
+        `{
+          allChildrenList(includeArchived: EXCLUSIVELY) {
+            id
+          }
+        }`,
+        { allChildrenList: iderize(1002, 2002) },
+      ),
+    );
+  });
+
+  describe("children of parents", () => {
+    test(
+      "Omits archived parents and children by default",
+      check(
+        `{
+          allParentsList {
+            id
+            childrenByParentIdList {
+              id
+            }
+          }
+        }`,
+        {
+          allParentsList: [
             {
               id: 1,
-              childrenByParentId: { nodes: iderize(1001) },
+              childrenByParentIdList: iderize(1001),
             },
           ],
         },
-      },
-    ),
-  );
+      ),
+    );
 
-  test(
-    "Omits archived parents and children when NO",
-    check(
-      `{
-        allParents(includeArchived: NO) {
-          nodes {
+    test(
+      "Omits archived parents and children when NO",
+      check(
+        `{
+          allParentsList(includeArchived: NO) {
             id
-            childrenByParentId {
-              nodes {
-                id
-              }
+            childrenByParentIdList {
+              id
             }
           }
-        }
-      }`,
-      {
-        allParents: {
-          nodes: [
+        }`,
+        {
+          allParentsList: [
             {
               id: 1,
-              childrenByParentId: { nodes: iderize(1001) },
+              childrenByParentIdList: iderize(1001),
             },
           ],
         },
-      },
-    ),
-  );
+      ),
+    );
 
-  test(
-    "Includes all parents, and treats children as INHERIT (all children of an archived parent, but only the unarchived children of an unarchived parent) when YES",
-    check(
-      `{
-        allParents(includeArchived: YES) {
-          nodes {
+    test(
+      "Includes all parents, and treats children as INHERIT (all children of an archived parent, but only the unarchived children of an unarchived parent) when YES",
+      check(
+        `{
+          allParentsList(includeArchived: YES) {
             id
-            childrenByParentId {
-              nodes {
-                id
-              }
+            childrenByParentIdList {
+              id
             }
           }
-        }
-      }`,
-      {
-        allParents: {
-          nodes: [
+        }`,
+        {
+          allParentsList: [
             {
               id: 1,
-              childrenByParentId: { nodes: iderize(1001) },
+              childrenByParentIdList: iderize(1001),
             },
             {
               id: 2,
-              childrenByParentId: { nodes: iderize(2001, 2002) },
+              childrenByParentIdList: iderize(2001, 2002),
             },
           ],
         },
-      },
-    ),
-  );
+      ),
+    );
 
-  test(
-    "Includes only archived parents (and all their children) when EXCLUSIVELY",
-    check(
-      `{
-        allParents(includeArchived: EXCLUSIVELY) {
-          nodes {
+    test(
+      "Includes only archived parents (and all their children) when EXCLUSIVELY",
+      check(
+        `{
+          allParentsList(includeArchived: EXCLUSIVELY) {
             id
-            childrenByParentId {
-              nodes {
-                id
-              }
+            childrenByParentIdList {
+              id
             }
           }
-        }
-      }`,
-      {
-        allParents: {
-          nodes: [
+        }`,
+        {
+          allParentsList: [
             {
               id: 2,
-              childrenByParentId: { nodes: iderize(2001, 2002) },
+              childrenByParentIdList: iderize(2001, 2002),
             },
           ],
         },
-      },
-    ),
-  );
+      ),
+    );
+  });
 });
