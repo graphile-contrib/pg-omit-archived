c387f7d8bfe8ca3c49590da607d4e75cd6fc154c
Benjie Gillam <benjie@jemjie.com>
2025-07-13 10:46:52 +0100

    Upgrade ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import graphileExport from "eslint-plugin-graphile-export";
+import jest from "eslint-plugin-jest";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  //"plugin:jest/recommended",
+  prettier, // not a plugin, just a config object
+  graphileExport.configs.recommended,
+  {
+    plugins: {
+      //"@typescript-eslint",
+      jest,
+      //"graphile-export"
+    },
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        jasmine: false,
+        ...globals.jest,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "jest/expect-expect": ["off"],
+      "@typescript-eslint/no-namespace": ["off"],
+    },
+  },
+]);
